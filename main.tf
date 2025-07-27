18d73e954b10fb64693533b856120d525a68978f
Shivy104 <108394334+Shivy104@users.noreply.github.com>
2025-07-27 09:56:42 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {
+    
+  }
+  subscription_id = "212ab218-5df9-48bc-afac-cd9389093c1b"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+}
