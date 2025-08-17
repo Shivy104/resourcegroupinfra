f0d27e8300ff5bdaef8ef994c6ebb04634434b63
Shivy104 <108394334+Shivy104@users.noreply.github.com>
2025-08-17 19:08:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,3 +19,9 @@ resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "example2"
+  location = "West Europe"
+}
+
